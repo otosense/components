042ee3e373854993e08a3c94856ab6fa8d1e11e5
Steve Herzog <steve.herzog@analog.com>
2021-11-18 22:33:36 -0600

    Replace npm with yarn, update some dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 // import StyledTextField from './components/StyledTextField';
 // import StyledSelectField from './components/StyledSelectField';
-import Header from './components/Header'
+import Header from './components/Header';
+import otosenseTheme from './shared/theme';
 
 export {
   // StyledSelectField,
   // StyledTextField,
   Header,
+  otosenseTheme,
 };
--- src/shared/theme.ts
+++ src/shared/theme.ts
@@ -22,6 +22,7 @@ declare module '@mui/material/styles' {
     critical: Palette['primary'];
     header: Palette['primary'];
   }
+
   interface PaletteOptions {
     cancel: PaletteOptions['primary'];
     critical: PaletteOptions['primary'];
@@ -30,99 +31,99 @@ declare module '@mui/material/styles' {
 }
 
 const otosenseTheme = {
-    palette: {
-        primary: {
-          main: '#009fbd',
-          // dark: '#101820',
-          contrastText: '#fff',
-        },
-        secondary: {
-          main: '#CBD4EB',
-          contrastText: '#003965',
-        },
-        cancel: {
-          main: '#C6D6DA',
-          light: '#fff',
-          dark: '#9CB3B9',
-          contrastText: '#146474',
-        },
-        critical: {
-          main: '#CC4734',
-          contrastText: '#fff',
-          light: '#E9D0D9',
-          dark: '#8C2026',
-        },
-        error: {
-          main: '#f56d4d',
-        },
-        warning: {
-          main: '#fff847',
-        },
-        info: {
-          main: '#47afff',
-        },
-        success: {
-          main: '#1db381',
-        },
-        background: {
-          paper: '#fff',
-          default: '',
-        },
-        text: {
-          primary: '#101820',
-          secondary: '#146474',
-          disabled: '#BDBFCA',
-        },
+  palette: {
+    primary: {
+      main: '#009fbd',
+      // dark: '#101820',
+      contrastText: '#fff',
+    },
+    secondary: {
+      main: '#CBD4EB',
+      contrastText: '#003965',
+    },
+    cancel: {
+      main: '#C6D6DA',
+      light: '#fff',
+      dark: '#9CB3B9',
+      contrastText: '#146474',
+    },
+    critical: {
+      main: '#CC4734',
+      contrastText: '#fff',
+      light: '#E9D0D9',
+      dark: '#8C2026',
+    },
+    error: {
+      main: '#f56d4d',
+    },
+    warning: {
+      main: '#fff847',
+    },
+    info: {
+      main: '#47afff',
+    },
+    success: {
+      main: '#1db381',
+    },
+    background: {
+      paper: '#fff',
+      default: '',
+    },
+    text: {
+      primary: '#101820',
+      secondary: '#146474',
+      disabled: '#BDBFCA',
+    },
+  },
+  typography: {
+    htmlFontSize: 16,
+    h1: {
+      fontFamily: '',
+      fontSize: 32,
+      lineHeight: '40px',
+      // letterSpacing: '',
+    },
+    h2: {
+      fontFamily: '',
+      fontSize: 24,
+      lineHeight: '32px',
+      textTransform: 'capitalize',
+      // letterSpacing: '',
+    },
+    h3: {
+      'fontFamily': '',
+      'fontSize': 20,
+      'lineHeight': '24px',
+      '@media (min-width:600px)': {
+        fontSize: '1.5rem',
       },
-    typography: {
-        htmlFontSize: 16,
-        h1: {
-          fontFamily: '',
-          fontSize: 32,
-          lineHeight: '40px',
-          // letterSpacing: '',
-        },
-        h2: {
-          fontFamily: '',
-          fontSize: 24,
-          lineHeight: '32px',
-          textTransform: 'capitalize',
-          // letterSpacing: '',
-        },
-        h3: {
-          'fontFamily': '',
-          'fontSize': 20,
-          'lineHeight': '24px',
-          '@media (min-width:600px)': {
-            fontSize: '1.5rem',
-          },
-          // letterSpacing: '',
-        },
-        h4: {
-          fontFamily: '',
-          fontSize: 18,
-          lineHeight: '24px',
-        },
-        body1: {
-          fontFamily: '',
-          fontSize: 16,
-          lineHeight: '',
-          letterSpacing: '',
-        },
-        button: {
-          fontFamily: '"IBMPlexSans-SemiBold", sans-serif',
-          fontSize: 14,
-          // lineHeight: '',
-          // letterSpacing: '',
-        },
-      },
-    shape: {
-        borderRadius: 0,
-      },
-    spacing: 24,
-    components: {
-        MuiCssBaseline: {
-          styleOverrides: `
+      // letterSpacing: '',
+    },
+    h4: {
+      fontFamily: '',
+      fontSize: 18,
+      lineHeight: '24px',
+    },
+    body1: {
+      fontFamily: '',
+      fontSize: 16,
+      lineHeight: '',
+      letterSpacing: '',
+    },
+    button: {
+      fontFamily: '"IBMPlexSans-SemiBold", sans-serif',
+      fontSize: 14,
+      // lineHeight: '',
+      // letterSpacing: '',
+    },
+  },
+  shape: {
+    borderRadius: 0,
+  },
+  spacing: 24,
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: `
             @font-face {
               font-family: 'IBMPlexSans-Regular';
               font-style: normal;
@@ -156,100 +157,100 @@ const otosenseTheme = {
               unicodeRange: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF;
             }
           `,
-        },
-        MuiButton: {
-          styleOverrides: {
-            root: {
-              textTransform: 'capitalize',
-              width: 'auto',
-            },
-          },
-          defaultProps: {
-            variant: 'contained',
-            disableElevation: true,
-            // disableRipple: true,
-          },
-          variants: [
-            {
-              props: { variant: 'contained', size: 'large'},
-              style: {
-                fontSize: 20,
-                minHeight: 60,
-                lineHeight: 1.5,
-                letterSpacing: 2,
-                minWidth: 140,
-              },
-            }, {
-              props: { variant: 'contained', size: 'medium'},
-              style: {
-                fontSize: 14,
-                minHeight: 40,
-              },
-            },
-          ],
-        },
-        MuiFormControl: {
-          variants: [
-            {
-              props: { variant: 'filled', size: 'medium'},
-              style: { marginRight: 22, marginBottom: 25, width: 315},
-            },
-          ],
-        },
-        MuiTextField: {
-          defaultProps: {
-            variant: 'standard',
-          },
-        },
-        MuiTypography: {
-          variants: [
-            {
-              props: { variant: 'overline'},
-              style: { textTransform: 'capitalize', marginBottom: '7px', lineHeight: 'initial', fontSize: '14px'},
-            },
-          ],
-          defaultProps: {
-            variantMapping: {
-              h1: 'h1',
-              h2: 'h2',
-              h3: 'h3',
-              h4: 'h4',
-              h5: 'h5',
-              h6: 'h6',
-              subtitle1: 'h2',
-              subtitle2: 'span',
-              body1: 'span',
-              body2: 'p',
-            },
-          },
-        },
-        MuiTabs: {
-          styleOverrides: {
-            root: {
-              textTransform: 'capitalize',
-              fontFamily: '"OpenSans-Regular", sans-serif',
-            },
-          },
-        },
-        MuiTab: {
-          styleOverrides: {
-            root: {
-              textTransform: 'capitalize',
-              fontFamily: '"OpenSans-Regular", sans-serif',
-              fontSize: 18,
-              color: '#101820',
-              paddingLeft: 0,
-            },
-          },
-        },
-        MuiDrawer: {
-          styleOverrides: {
-            paper: {
-              minWidth: 424,
-            },
-          },
+    },
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          textTransform: 'capitalize',
+          width: 'auto',
         },
       },
+      defaultProps: {
+        variant: 'contained',
+        disableElevation: true,
+        // disableRipple: true,
+      },
+      variants: [
+        {
+          props: {variant: 'contained', size: 'large'},
+          style: {
+            fontSize: 20,
+            minHeight: 60,
+            lineHeight: 1.5,
+            letterSpacing: 2,
+            minWidth: 140,
+          },
+        }, {
+          props: {variant: 'contained', size: 'medium'},
+          style: {
+            fontSize: 14,
+            minHeight: 40,
+          },
+        },
+      ],
+    },
+    MuiFormControl: {
+      variants: [
+        {
+          props: {variant: 'filled', size: 'medium'},
+          style: {marginRight: 22, marginBottom: 25, width: 315},
+        },
+      ],
+    },
+    MuiTextField: {
+      defaultProps: {
+        variant: 'standard',
+      },
+    },
+    MuiTypography: {
+      variants: [
+        {
+          props: {variant: 'overline'},
+          style: {textTransform: 'capitalize', marginBottom: '7px', lineHeight: 'initial', fontSize: '14px'},
+        },
+      ],
+      defaultProps: {
+        variantMapping: {
+          h1: 'h1',
+          h2: 'h2',
+          h3: 'h3',
+          h4: 'h4',
+          h5: 'h5',
+          h6: 'h6',
+          subtitle1: 'h2',
+          subtitle2: 'span',
+          body1: 'span',
+          body2: 'p',
+        },
+      },
+    },
+    MuiTabs: {
+      styleOverrides: {
+        root: {
+          textTransform: 'capitalize',
+          fontFamily: '"OpenSans-Regular", sans-serif',
+        },
+      },
+    },
+    MuiTab: {
+      styleOverrides: {
+        root: {
+          textTransform: 'capitalize',
+          fontFamily: '"OpenSans-Regular", sans-serif',
+          fontSize: 18,
+          color: '#101820',
+          paddingLeft: 0,
+        },
+      },
+    },
+    MuiDrawer: {
+      styleOverrides: {
+        paper: {
+          minWidth: 424,
+        },
+      },
+    },
+  },
 };
 export default otosenseTheme;
 
